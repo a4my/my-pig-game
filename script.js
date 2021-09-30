dbd6939af24971f1247c2e57c3168d43baa05b3d
Alex Fourmy <alexfourmy@gmail.com>
2021-09-30 11:58:31 +0200

    refactored the code to work on the css media queries

--- script.js
+++ script.js
@@ -27,14 +27,15 @@ const displayTarget = document.getElementById('score-target')
 
 //Display score target
 const btnSet = document.querySelector('.btn--set')
-let scoreTarget = 20;
+let scoreTarget = 20
 displayTarget.textContent = scoreTarget
 
 //Update score board functionality
 let gameWon = [0, 0]
-const updateScore = function() {
+const updateScore = function () {
   gameWon[activePlayer] += 1
-  document.getElementById(`field-game-${activePlayer}`).textContent = gameWon[activePlayer]
+  document.getElementById(`field-game-${activePlayer}`).textContent =
+    gameWon[activePlayer]
 }
 
 // Player 1 naming functionality
@@ -49,29 +50,27 @@ btnSet1.addEventListener('click', function () {
   let selectName1 = document.getElementById('select-name-1').value
   name1El.textContent = selectName1
   fieldPlayer1.textContent = selectName1
-
 })
 
 // Score target functionality
-btnSet.addEventListener('click', function(){
+btnSet.addEventListener('click', function () {
   let selectedScore = document.querySelector('.select-score').value
   scoreTarget = parseInt(selectedScore)
   displayTarget.textContent = scoreTarget
 })
 
-
 //Starting conditions
 
 let scores, playing, activePlayer, currentScore
 
-const init = function() {
+const init = function () {
   player0El.classList.remove('player--winner')
   player1El.classList.remove('player--winner')
-  
+
   diceEl.classList.add('hidden')
-  
+
   activePlayer = Math.trunc(Math.random() * 2)
-  if(activePlayer === 0) {
+  if (activePlayer === 0) {
     player0El.classList.add('player--active')
     player1El.classList.remove('player--active')
   } else {
@@ -91,46 +90,52 @@ const init = function() {
 
 init()
 
-const rollDice = function() {
-  if(playing) {
+const rollDice = function () {
+  if (playing) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1
     // 2. Display dice
     diceEl.classList.remove('hidden')
     diceEl.src = `images/side-${dice}.png`
     // 3. Check for rolled 1, if true, switch to next player
-    if(dice !== 1) {
+    if (dice !== 1) {
       currentScore += dice
-      document.getElementById(`current--${activePlayer}`).textContent = currentScore
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore
     } else {
       switchPlayer()
     }
   }
 }
 
-const holdScore = function() {
+const holdScore = function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer]
 
     // 2. Check if player's score is >= 30
-    if(scores[activePlayer] >= scoreTarget) {
+    if (scores[activePlayer] >= scoreTarget) {
       // Finish the game
       playing = false
       diceEl.classList.add('hidden')
-      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
-      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner')
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active')
       updateScore()
       // document.getElementById(`name--${activePlayer}`).textContent = 'YOU WIN!'
     } else {
-    // Switch to the next player
-    switchPlayer()
+      // Switch to the next player
+      switchPlayer()
     }
   }
 }
 
-const switchPlayer = function() {
+const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0
   currentScore = 0
   activePlayer = activePlayer === 0 ? 1 : 0
@@ -138,7 +143,7 @@ const switchPlayer = function() {
   player1El.classList.toggle('player--active')
 }
 
-if(activePlayer === 0) {
+if (activePlayer === 0) {
   player0El.classList.add('player--active')
 } else {
   player1El.classList.add('player--active')
@@ -158,21 +163,21 @@ btnNew.addEventListener('click', init)
 //   if(newKey.key === 'Shift' && modal.classList.contains('hidden') && modal1.classList.contains('hidden')) {
 //    init()
 //  }
-// })  
+// })
 
 // // Press ENTER button = Roll Dice
 // document.addEventListener('keydown', function(rollKey) {
 //   if(rollKey.key === 'Enter' && modal.classList.contains('hidden') && modal1.classList.contains('hidden')) {
 //    rollDice()
 //  }
-// }) 
+// })
 
 // // Press Space button = Hold Current Score
 // document.addEventListener('keydown', function(holdKey) {
 //   if(holdKey.code === 'Space' && modal.classList.contains('hidden') && modal1.classList.contains('hidden')) {
 //    holdScore()
-//  }     
-// })  
+//  }
+// })
 
 // Modal Button Rules
 
@@ -181,31 +186,29 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
-const openModal = function() {
+const openModal = function () {
   modal.classList.remove('hidden')
   overlay.classList.remove('hidden')
 }
 
-const closeModal = function() {
+const closeModal = function () {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 }
 
-for(let i = 0; i < btnsOpenModal.length; i++) {
+for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal)
   btnCloseModal.addEventListener('click', closeModal)
   overlay.addEventListener('click', closeModal)
 }
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
   // console.log(e.key)
 
-  if(e.key === 'Escape' && !modal.classList.contains('hidden')) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal()
   }
 })
-
- 
 
 // Modal Button Options
 
@@ -214,26 +217,26 @@ const overlay1 = document.querySelector('.overlay-1')
 const btnCloseModal1 = document.querySelector('.close-modal-1')
 const btnsOpenModal1 = document.querySelectorAll('.show-modal-1')
 
-const openModal1 = function() {
+const openModal1 = function () {
   modal1.classList.remove('hidden')
   overlay1.classList.remove('hidden')
 }
 
-const closeModal1 = function() {
+const closeModal1 = function () {
   modal1.classList.add('hidden')
   overlay1.classList.add('hidden')
 }
 
-for(let x = 0; x < btnsOpenModal1.length; x++) {
+for (let x = 0; x < btnsOpenModal1.length; x++) {
   btnsOpenModal1[x].addEventListener('click', openModal1)
   btnCloseModal1.addEventListener('click', closeModal1)
   overlay1.addEventListener('click', closeModal1)
 }
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   // console.log(e.key)
 
-  if(event.key === 'Escape' && !modal1.classList.contains('hidden')) {
+  if (event.key === 'Escape' && !modal1.classList.contains('hidden')) {
     closeModal1()
   }
 })
